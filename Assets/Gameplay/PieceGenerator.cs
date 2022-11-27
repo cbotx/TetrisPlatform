72d62a3335096ea900eae9ef13e142240e619f04
cbotx <szfxcyx@126.com>
2022-11-27 14:34:22 +0800

    amend

--- Assets/Gameplay/PieceGenerator.cs
+++ Assets/Gameplay/PieceGenerator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using Assets.Skins;
+using Assets.Definitions;
 public class PieceGenerator
 {
     public Transform field;
@@ -26,7 +27,12 @@ public class PieceGenerator
 
         Vector3[] blocks = pieceType.BaseShape.ToVector3Array();
 
-        List<Tile> tiles = skin.GetPieceTiles(GameDefinitions.Tetrominos[pieceId].BaseShape, isGhost ? 7 : pieceId);
+        List<Tile> tiles = skin.GetPieceTiles(GameDefinitions.Tetrominos[pieceId].BaseShape, pieceId);
+        List<Tile> ghostTiles = null;
+        if (isGhost)
+        {
+            ghostTiles = skin.GetPieceTiles(GameDefinitions.Tetrominos[pieceId].BaseShape, 7);
+        }
         for (int i = 0; i < 4; ++i)
         {
             GameObject tileObject = new();
@@ -35,6 +41,15 @@ public class PieceGenerator
             SpriteRenderer renderer = tileObject.AddComponent<SpriteRenderer>();
             renderer.sprite = tiles[i].sprite;
             renderer.sortingOrder = isGhost ? 0 : 1;
+
+            if (isGhost)
+            {
+                renderer.material = MaterialDefinitions.material_AlphaMask;
+                renderer.material.SetTexture("_Alpha", ghostTiles[i].sprite.texture);
+                float offsetUnit = 1.0f * skin.TileWidth / ghostTiles[i].sprite.texture.width;
+                renderer.material.SetTextureScale("_Alpha", new Vector2(2, 2));
+                renderer.material.SetTextureOffset("_Alpha", new Vector2(-offsetUnit * SkinDefinitions.ConnectedTexturePosition[pieceId, 0], offsetUnit * SkinDefinitions.ConnectedTexturePosition[pieceId, 1]));
+            }
 
             tileObject.transform.SetParent(pieceGameObject.transform);
             tileObject.transform.position = blocks[i];
